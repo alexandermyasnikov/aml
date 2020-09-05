ce0646ae56038a6bea109ee2d9d34c44d4c21529
Alexander Myasnikov <myasnikov.alexander.s@gmail.com>
2020-09-05 09:29:01 +0300

    fix env building

--- aml/include/env.h
+++ aml/include/env.h
@@ -17,11 +17,13 @@ namespace aml::env_n {
     std::string show() const;
   };
 
+  using var_info_sptr_t = std::shared_ptr<var_info_t>;
+
 
 
   struct env_t : std::enable_shared_from_this<env_t> {
     using key_t = std::string;
-    using val_t = std::shared_ptr<var_info_t>;
+    using val_t = var_info_sptr_t;
     using vars_t = std::deque<val_t>;
 
     std::shared_ptr<env_t> parent;
@@ -31,8 +33,37 @@ namespace aml::env_n {
 
     env_t(std::shared_ptr<env_t> parent = nullptr) : parent(parent) { }
 
-    val_t def(const auto& key, auto pvars);
-    val_t get(const auto& key, auto pvar, auto pvars) const;
+    val_t def(const auto& key, auto pvars) {
+      auto it = std::find_if((this->*pvars).begin(), (this->*pvars).end(),
+          [&key](auto var) { return key == var->name; });
+      if (it != (this->*pvars).end())
+        throw utils_n::fatal_error_t("env_t: '" + key + "' is exists");
+
+      (this->*pvars).push_back(std::make_shared<var_info_t>());
+      (this->*pvars).back()->id = id++;
+      (this->*pvars).back()->name = key;
+      return (this->*pvars).back();
+    }
+
+    val_t get(const auto& key, auto pvar, auto pvars) const {
+      auto env = this->shared_from_this();
+      while (env) {
+        auto it = std::find_if((*env.*pvars).begin(), (*env.*pvars).end(),
+            [&key, pvar](auto var) { return key == (*var).*pvar; });
+        if (it != (*env.*pvars).end()) {
+          return *it;
+        }
+        env = env->parent;
+      }
+
+      std::string key_str;
+      if constexpr (std::is_arithmetic<decltype(key)>::value) {
+        key_str = std::to_string(key);
+      } else {
+        key_str = key;
+      }
+      throw utils_n::fatal_error_t("env_t: '" + key_str + "' is not exists");
+    }
 
     val_t def_func(const key_t& key) {
       return def(key, &env_t::funcs);
@@ -56,4 +87,6 @@ namespace aml::env_n {
 
     std::string show() const;
   };
+
+  using env_sptr_t = std::shared_ptr<env_t>;
 }
--- aml/src/env.cpp
+++ aml/src/env.cpp
@@ -12,38 +12,6 @@ namespace aml::env_n {
 
 
 
-  env_t::val_t env_t::def(const auto& key, auto pvars) {
-    auto it = std::find_if((this->*pvars).begin(), (this->*pvars).end(),
-        [&key](auto var) { return key == var->name; });
-    if (it != (this->*pvars).end())
-      throw utils_n::fatal_error_t("env_t: '" + key + "' is exists");
-
-    (this->*pvars).push_back(std::make_shared<var_info_t>());
-    (this->*pvars).back()->id = id++;
-    (this->*pvars).back()->name = key;
-    return (this->*pvars).back();
-  }
-
-  env_t::val_t env_t::get(const auto& key, auto pvar, auto pvars) const {
-    auto env = this->shared_from_this();
-    while (env) {
-      auto it = std::find_if((*env.*pvars).begin(), (*env.*pvars).end(),
-          [&key, pvar](auto var) { return key == (*var).*pvar; });
-      if (it != (*env.*pvars).end()) {
-        return *it;
-      }
-      env = env->parent;
-    }
-
-    std::string key_str;
-    if constexpr (std::is_arithmetic<decltype(key)>::value) {
-      key_str = std::to_string(key);
-    } else {
-      key_str = key;
-    }
-    throw utils_n::fatal_error_t("env_t: '" + key_str + "' is not exists");
-  }
-
   std::string env_t::show() const {
     std::string str;
     auto env = this->shared_from_this();
