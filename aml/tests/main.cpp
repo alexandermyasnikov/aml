382641a8b4bd96b9f842543499afe22366e21de9
Alexander Myasnikov <myasnikov.alexander.s@gmail.com>
2020-09-06 18:45:23 +0300

    added tests

--- aml/tests/main.cpp
+++ aml/tests/main.cpp
@@ -2,44 +2,60 @@
 #include <catch2/catch.hpp>
 #include "aml.h"
 
-auto sum = [](auto a, auto b) { return a + b; };
 
-TEST_CASE("stmt int", "[int]") {
 
-  std::string input = "(int 1)";
-  std::string output = "1";
-  std::string code = {};
+struct test_case_t {
+  std::string name   = {};
+  std::string input  = {};
+  std::string output = {};
+  bool check_result  = true;
+};
 
+
+
+static inline std::vector<test_case_t> test_cases = {
   {
-    std::stringstream log;
-    bool success = aml::aml_n::compile(input, code, log);
-    REQUIRE(success);
-  }
+    .name   = "int 0",
+    .input  = "(int 0)",
+    .output = "0",
+  }, {
+    .name   = "int 1",
+    .input  = "(int 1)",
+    .output = "1",
+  }, {
+    .name   = "if then",
+    .input  = "(if (int 1) (int 10) (int -10))",
+    .output = "10",
+  }, {
+    .name   = "if else",
+    .input  = "(if (int 0) (int 10) (int -10))",
+    .output = "-10",
+  },
+};
 
-  {
-    std::stringstream log;
-    bool success = aml::aml_n::execute(code, log);
-    REQUIRE(success);
-    REQUIRE(output == log.str());
+
+
+TEST_CASE("core tests") {
+  for (const auto& test_case : test_cases) {
+    std::string name = "test_case " + test_case.name;
+    SECTION(name) {
+      std::string code;
+
+      {
+        std::stringstream log;
+        bool success = aml::aml_n::compile(test_case.input, code, log);
+        REQUIRE(success);
+      }
+
+      {
+        std::stringstream log;
+        bool success = aml::aml_n::execute(code, log);
+        REQUIRE(success);
+        if (test_case.check_result) {
+          REQUIRE(test_case.output == log.str());
+        }
+      }
+    }
   }
 }
 
-TEST_CASE("stmt int 2", "[int]") {
-
-  std::string input = "(int -1)";
-  std::string output = "-1";
-  std::string code = {};
-
-  {
-    std::stringstream log;
-    bool success = aml::aml_n::compile(input, code, log);
-    REQUIRE(success);
-  }
-
-  {
-    std::stringstream log;
-    bool success = aml::aml_n::execute(code, log);
-    REQUIRE(success);
-    REQUIRE(output == log.str());
-  }
-}
